f77d1328f0dffcd525c09b13cda1c564bcdeadb0
Kitare <221355@astanait.edu.kz>
2024-09-07 00:44:08 -0400

    link fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,12 +22,12 @@ let header = $(`
 
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="/index.html">Home</a></li>
-    <li class="nav-item nav-item-hover"><a class="nav-link" href="/skills.html">Skills</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="/experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="/devProjects.html">Dev Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="/design.html">Design Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="/blog.html">Blog</a></li>
+<li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/index.html">Home</a></li>
+    <li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/skills.html">Skills</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/experience.html">Experience</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/devProjects.html">Dev Projects</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/design.html">Design Projects</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/portfolio/blog.html">Blog</a></li>
     <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
